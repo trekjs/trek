b62091aa1cbc1bca14cb48897be93e6e674d3279
fundon <cfddream@gmail.com>
2015-03-08 19:22:28 +0800

    examples: remove comments

--- examples/hello-world/config/application.js
+++ examples/hello-world/config/application.js
@@ -3,7 +3,6 @@ export default (config) => {
     root: config.viewsPath
   }, true);
 
-  console.log(config.paths.get('config/locales').path);
   config.set('i18n', {
     directory: config.paths.get('config/locales').path,
     locales: ['zh-CN', 'en', 'zh-tw'],
