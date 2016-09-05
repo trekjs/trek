bc40fc2d18cb0ef668d8a3671d4a3f079a4b329e
Fangdun Cai (Fundon) <cfddream@gmail.com>
2016-09-06 00:15:45 +0800

    test: move request response to trek-engine

--- test/request/secure.js
+++
@@ -1,8 +0,0 @@
-import test from 'ava'
-import { request } from '../helpers/context'
-
-test('req.secure should return true when encrypted', async t => {
-  const req = await request()
-  req.req.socket = { encrypted: true }
-  t.true(req.secure)
-})
