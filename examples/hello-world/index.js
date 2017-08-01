9f9fb13e161fb47b5c65b289d083f30d3b89abf2
Fangdun Cai <cfddream@gmail.com>
2017-08-02 06:56:45 +0800

    prettier

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -1,50 +1,48 @@
-const { Engine: Trek, Router } = require('../../lib')(
-  /* eslint no-unexpected-multiline: 0 */
-  async () => {
-    const app = new Trek()
+import { Engine as Trek, Router } from '../../lib'
+;(async () => {
+  const app = new Trek()
 
-    const router = new Router()
+  const router = new Router()
 
-    router.get('/', async ({ res }) => {
-      res.send(200, 'Hello, Trek!')
+  router.get('/', async ({ res }) => {
+    res.send(200, 'Hello, Trek!')
+  })
+
+  router.get('/startrek', async ({ res }) => {
+    res.type = 'html'
+    res.send(200, Buffer.from('Hello, Star Trek!'))
+  })
+
+  router.post('/', async ({ res }) => {
+    res.send(200, {
+      status: 'ok',
+      message: 'success'
     })
+  })
 
-    router.get('/startrek', async ({ res }) => {
-      res.type = 'html'
-      res.send(200, Buffer.from('Hello, Star Trek!'))
-    })
+  app.use(async ({ req, res }, next) => {
+    const start = new Date()
+    await next()
+    const ms = new Date() - start
+    console.log(`${ms}ms`)
+  })
 
-    router.post('/', async ({ res }) => {
-      res.send(200, {
-        status: 'ok',
-        message: 'success'
-      })
-    })
-
-    app.use(async ({ req, res }, next) => {
-      const start = new Date()
-      await next()
-      const ms = new Date() - start
-      console.log(`${ms}ms`)
-    })
-
-    app.use(async ({ req, res }, next) => {
-      const route = router.find(req.method, req.path)
-      if (route) {
-        const [handler] = route
-        if (handler !== undefined) {
-          await handler({ req, res })
-          return
-        }
+  app.use(async ({ req, res }, next) => {
+    const route = router.find(req.method, req.path)
+    if (route) {
+      const [handler] = route
+      if (handler !== undefined) {
+        await handler({ req, res })
+        return
       }
-      await next()
-    })
+    }
+    await next()
+  })
 
-    app.use(async ({ res }) => {
-      res.status = 404
-      res.end()
-    })
+  app.use(async ({ res }) => {
+    res.status = 404
+    res.end()
+  })
 
-    app.run(3000)
-  }
-)().catch(console.error)
+  app.run(3000)
+})().catch(console.error)
--- examples/startrek/app.js
+++ examples/startrek/app.js
@@ -1,40 +1,38 @@
-const Trek = require('../../lib')(
-  /* eslint no-unexpected-multiline: 0 */
-  async () => {
-    const app = new Trek()
+import Trek from '../../lib'
+;(async () => {
+  const app = new Trek()
 
-    app.paths.set('app', { single: true })
-    app.paths.set('app/plugins', { glob: 'app/plugins/index.js', single: true })
-    app.paths.set('app/controllers', { glob: 'app/controllers/*.js' })
+  app.paths.set('app', { single: true })
+  app.paths.set('app/plugins', { glob: 'app/plugins/index.js', single: true })
+  app.paths.set('app/controllers', { glob: 'app/controllers/*.js' })
 
-    await app.bootUp()
+  await app.bootUp()
 
-    app.use(async ({ logger, rawReq, rawRes }, next) => {
-      logger.info(rawReq)
-      await next()
-      logger.info(rawRes)
-    })
+  app.use(async ({ logger, rawReq, rawRes }, next) => {
+    logger.info(rawReq)
+    await next()
+    logger.info(rawRes)
+  })
 
-    app.use(async ({ cookies }, next) => {
-      cookies.set('name', 'trek')
-      await next()
-    })
+  app.use(async ({ cookies }, next) => {
+    cookies.set('name', 'trek')
+    await next()
+  })
 
-    app.use(ctx => {
-      if (ctx.req.path === '/') {
-        return ctx.res.send(200, 'Star Trek!')
-      } else if (ctx.req.path === '/error') {
-        throw new Error('Nothing')
-      }
-      // Something else return 404
-      ctx.cookies.set('name', null)
-      ctx.res.send(404)
-    })
+  app.use(ctx => {
+    if (ctx.req.path === '/') {
+      return ctx.res.send(200, 'Star Trek!')
+    } else if (ctx.req.path === '/error') {
+      throw new Error('Nothing')
+    }
+    // Something else return 404
+    ctx.cookies.set('name', null)
+    ctx.res.send(404)
+  })
 
-    app.on('error', err => {
-      app.logger.error(err)
-    })
+  app.on('error', err => {
+    app.logger.error(err)
+  })
 
-    await app.run(3000)
-  }
-)().catch(console.error)
+  await app.run(3000)
+})().catch(console.error)
